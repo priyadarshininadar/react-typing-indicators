c5c18614b05b082ceaf30bea308a1e9c6b3b03d2
Priyadarshini Nadar <priyadarshini.nadar>
2020-11-10 12:48:00 +0530

    server.js to serve build/index.html (for glitch)

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('build'));
+
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + '/build/index.html');
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
